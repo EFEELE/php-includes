fcb918d2a5fbdbbc8ac800292b92db0580b815e7
Fernando Lopez <34411748+EFEELE@users.noreply.github.com>
2021-06-22 19:11:15 -0500

    First

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    </body>
+<footer>
+    <span>&copy; 2021 - EFEELE</span>
+</footer>
+
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php'); ?>
+
+<div class="content">
+    <h1>Ejercicio Includes</h1>
+</div>
+
+<?php include('includes/footer.php'); ?>
